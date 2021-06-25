dce82f7cede61e7990db73a40058bf9cae1331ef
hanabi1024 <91681869+hanabi1024@users.noreply.github.com>
2021-06-25 17:16:29 +0000

    Update 1.md

--- Machine_Vision/Vehicle_Detection_Aerial_Images/Software/HWdetect.m
+++ Machine_Vision/Vehicle_Detection_Aerial_Images/Software/HWdetect.m
@@ -37,4 +37,4 @@ imshow(thresholdedImage);
 
 %2- Morphlogical operations:
 BthresholdedImage = im2bw(thresholdedImage);
-%2-1- clean: Removes isolated pixels (individual 1's t
+%2-1- clean: Removes isolated pixels (individual 1's th
