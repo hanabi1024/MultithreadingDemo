5670130757fa779eefa7c394a24121ded08819a4
hanabi1024 <91681869+hanabi1024@users.noreply.github.com>
2022-05-16 13:53:38 +0000

    chore(package): update @types/node to version 9.3.0

--- Machine_Vision/Vehicle_Detection_Aerial_Images/Software/HWdetect.m
+++ Machine_Vision/Vehicle_Detection_Aerial_Images/Software/HWdetect.m
@@ -56,4 +56,4 @@ figure(196);
 imshow(filledMajoredCleanedBthresholdedImage);
 %2-4- erode
 erodedFilledMajoredCleanedBthresholdedImage = bwmorph(filledMajoredCleanedBthresholdedImage,'erode');
-figure(1
+figure(195
