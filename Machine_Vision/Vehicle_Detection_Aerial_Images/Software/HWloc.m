434a65b1fdcf1c23bee53cebde68d93e70f4b2d7
hanabi1024 <91681869+hanabi1024@users.noreply.github.com>
2023-03-12 20:22:23 +0000

    clean code

--- Machine_Vision/Vehicle_Detection_Aerial_Images/Software/HWloc.m
+++ Machine_Vision/Vehicle_Detection_Aerial_Images/Software/HWloc.m
@@ -2,3 +2,6 @@
 prompt = {'Enter the path of original image: '}; 
 title = 'File Path';
 location = inputdlg(prompt, title); 
+location1 = char(location); % type conversion from cellstring to string
+
+% if (strcmp(location1,'')==1)
